65e587be9279587d833248bcedbe25e93bc8b183
saridasofianiaraidah <saridasofianiarasidah23@mhs.politala.ac.id>
2025-01-02 19:49:43 +0800

    Update 2024_11_22_002602_create_transactions_table.php

--- database/migrations/2024_11_22_002602_create_transactions_table.php
+++ database/migrations/2024_11_22_002602_create_transactions_table.php
@@ -6,35 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateTransactionsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('transactions', function (Blueprint $table) {
-            $table->id(); // Kolom id
-            $table->unsignedBigInteger('agent_id'); // Kolom agent_id
-            $table->unsignedBigInteger('item_id'); // Kolom item_id
-            $table->integer('quantity'); // Kolom quantity
-            $table->decimal('unit_price', 8, 2); // Kolom unit_price
-            $table->decimal('total_price', 10, 2); // Kolom total_price
-            $table->decimal('discount', 8, 2)->default(0); // Kolom discount dengan nilai default 0
-            $table->string('payment_method'); // Kolom payment_method
-            $table->timestamps(); // Kolom created_at dan updated_at
-            
-            // Menambahkan foreign key constraints
-            $table->foreign('agent_id')->references('id')->on('agents')->onDelete('cascade');
-            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
-        });
-    }
+      public function up()
+        {
+            Schema::create('transactions', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('agent_id')->constrained()->onDelete('cascade');
+                $table->string('item_name');
+                $table->string('item_image')->nullable();
+                $table->decimal('netto', 10, 2);
+                $table->enum('unit', ['kg', 'g', 'mg', 'l']);
+                $table->foreignId('category_id')->constrained()->onDelete('cascade');
+                $table->decimal('unit_price', 10, 2);
+                $table->integer('quantity');
+                $table->decimal('discount', 10, 2)->nullable();
+                $table->decimal('total_price', 10, 2);
+                $table->date('purchase_date');
+                $table->enum('payment_method', ['cash', 'transfer']);
+                $table->timestamps();
+            });
+        }
+        
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('transactions');
